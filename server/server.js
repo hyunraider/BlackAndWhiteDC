edf12c8f3580f6d800cee7ed7a699932f63f2d81
Austin Bae <1551780@gmail.com>
2016-02-19 11:02:43 -0600

    fixes

--- server/server.js
+++ server/server.js
@@ -8,28 +8,13 @@ Meteor.methods({
         ImageInfo.remove({});
         Blog.remove({});
     },
+    removeCategory: function(){
+      CategoryList.remove({});
+    },
     showID: function(){
       console.log(Meteor.userId());
     },
     showSession: function(){
       console.log(Session.get('category'));
     }
-});
-
-Meteor.startup(function(){
-  console.log('b1');
-  CategoryList.remove({});
-  var categorylist = {};
-  for (var j=0; j<ImageInfo.find().fetch().length; j++){
-    var cat = ImageInfo.find().fetch()[j].category;
-    if (cat in categorylist){
-      categorylist[cat]++;
-    }else{
-      categorylist[cat] = 1;
-    }
-  }
-  console.log(categorylist);
-  for (var i in categorylist){
-    CategoryList.insert({name: i, number: " (" +categorylist[i] + ")"});
-  };
 });
